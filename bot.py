0804d68cc818192879afa2e6fd83a7fbb9622ba1
Diogo Gomes Cardoso <diogogomecardoso@gmail.com>
2022-04-28 01:37:07 +0100

    added music functionalities

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
+import discord
 from discord.ext import commands
 from decouple import config
+#import music
+#cogs = [music]
+bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
+#for i in range(len(cogs)):
+    #cogs[i].setup(bot)
 
-
-bot = commands.Bot('!')
-
+bot.load_extension("music")
 bot.load_extension("commands.crypto")
 bot.load_extension("commands.math")
 bot.load_extension("commands.reactions")
